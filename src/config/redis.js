71f2bf7d7a65c62badec625ac5d94d81a48863ae
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2026-01-19 17:39:52 +0530

    redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,7 @@
 import Redis from "ioredis";
 
 export const redisConnection = new Redis({
-  host: "127.0.0.1",
+  host: "payment_gateway_redis",
   port: 6379,
   maxRetriesPerRequest: null, // 🔥 REQUIRED by BullMQ
 });
